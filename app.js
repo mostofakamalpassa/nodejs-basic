06ed774c727fdf74c7ce5e5049b42a7fcefb86a7
mostofa kamal <mostofakamal1990@gmail.com>
2022-09-02 19:59:34 +0600

    routes add

--- app.js
+++ app.js
@@ -5,47 +5,21 @@ const bodyParser =require('body-parser');
 
 
 const app = express();
-
+const adminRoute = require("./routes/admins")
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req, res, next)=>{
-    console.log("hello Middleware 1");
-    next();
-});
-
-app.use((req, res, next)=>{
-    console.log("middleware 2 pss");
-   next();
-})
-
-
-app.use("/users",(req, res, next)=>{
-    res.send("<h2> Hello bangladesh</h2>")
-})
-
-app.use('/add-product', (req, res, next)=>{
-    res.send(`<form action="/add-product" method="Post">
-        <input type="text" name="product"/>
-        <button>Add product</button>
-    </form>`)
-})
-
-app.use('/product', (req, res, next)=>{
-    console.log(res.body);
-    res.redirect('/');
-})
-
+app.use(adminRoute);
 
 
 app.use('/',(req, res, next)=>{
-   // console.log(req);
-   res.send( "<h3>Hello My name Middleware  id i am bring every thins</h3>")
-   // next();
-});
+    // console.log(req);
+    res.send( "<h3>Hello My name Middleware  id i am bring every thins</h3>")
+    // next();
+ });
+
+
+
+
 // const server = http.createServer(app);
-
-
-
-
 // server.listen(5000);
 app.listen(5000);
---
+++ routes/admins.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/add-product", (req, res, next)=>{
+    res.send(
+        `
+            <form action='/product' method='post'>
+                <input type="text" name="title"/>
+                <input type="text" name='author'/>
+                <button>Save book</button>
+            </form>
+        `
+    )
+})
+
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+
+module.exports = router;
