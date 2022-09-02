2bb4b175e245ccc92ea18b898deabafe21279e3e
mostofa kamal <mostofakamal1990@gmail.com>
2022-09-02 19:44:19 +0600

    use middleware use

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const http = require('http');
 
-
+const bodyParser =require('body-parser');
 
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}))
+
 app.use((req, res, next)=>{
-    console.log(req);
+    console.log("hello Middleware 1");
     next();
 });
 
 app.use((req, res, next)=>{
+    console.log("middleware 2 pss");
+   next();
+})
+
+
+app.use("/users",(req, res, next)=>{
     res.send("<h2> Hello bangladesh</h2>")
 })
 
-const server = http.createServer(app);
+app.use('/add-product', (req, res, next)=>{
+    res.send(`<form action="/add-product" method="Post">
+        <input type="text" name="product"/>
+        <button>Add product</button>
+    </form>`)
+})
+
+app.use('/product', (req, res, next)=>{
+    console.log(res.body);
+    res.redirect('/');
+})
 
 
-server.listen(5000);
+
+app.use('/',(req, res, next)=>{
+   // console.log(req);
+   res.send( "<h3>Hello My name Middleware  id i am bring every thins</h3>")
+   // next();
+});
+// const server = http.createServer(app);
+
+
+
+
+// server.listen(5000);
+app.listen(5000);
